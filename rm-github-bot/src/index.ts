1bda6381d7e331e849b019774fcc35c9ab0ec47c
artlovecode <audun.mo@remarkable.no>
2020-08-13 11:01:36 +0200

    feat: init

---
+++ rm-github-bot/src/index.ts
@@ -0,0 +1,17 @@
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
+
+export = (app: Application) => {
+  app.on('issues.opened', async (context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    await context.github.issues.createComment(issueComment)
+  })
+
+  app.on('push', async context => {
+    console.log(context)
+  })
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
